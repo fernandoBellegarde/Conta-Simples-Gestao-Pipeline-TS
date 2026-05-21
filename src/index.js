fc680a12860ee99ab27fcff3c8fd15aeddff61bf
fernandoBellegarde <bellegardefernando@gmail.com>
2026-05-21 17:44:30 -0300

    feat(app): :tada: Projeto criado

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mensagem = "Meu projeto Serverless está nascendo!";
+console.log(mensagem);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const mensagem: string = "Projeto serveless conta simples gestão de pipeline";
+console.log(mensagem);
