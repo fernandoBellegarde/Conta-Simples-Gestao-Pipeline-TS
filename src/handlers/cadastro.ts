a5a3923fed5b0a3cb2ba1eb4b03b2fdde89b5616
fernandoBellegarde <bellegardefernando@gmail.com>
2026-05-21 19:19:48 -0300

    feat(handler): :sparkles: Enhance client creation handler to process and save client data

--- src/handlers/cadastro.ts
+++ src/handlers/cadastro.ts
@@ -1,6 +1,18 @@
-interface CriarClienteInput {
-	nome_empresa: string,
-	email_corp: string,
-	telefone: string,
-	potencial_gasto: number
+import { salvarCliente } from "../services/dynamo.js";
+export interface CriarClienteInput {
+  nome_empresa: string;
+  email_corp: string;
+  telefone: string;
+  potencial_gasto: number;
 }
+
+export const handler = async (event: any) => {
+  for (const registro of event.Records) {
+    try {
+      const dadosFormulario = JSON.parse(registro.body);
+      await salvarCliente(dadosFormulario);
+    } catch (err) {
+      console.error("Erro ao processar o formulario:", err);
+    }
+  }
+};
